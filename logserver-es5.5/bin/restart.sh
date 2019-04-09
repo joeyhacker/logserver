98ec9913df4fad55c1ba04c49e05a7c154d30fdc
joeyhacker <jhacker7@gmail.com>
2019-04-09 17:45:56 +0800

    update

--- logserver-es5.5/bin/restart.sh
+++ logserver-es5.5/bin/restart.sh
@@ -1,2 +1,14 @@
 #! /bin/sh
 
+if [ -f pid ];then
+ cat pid | xargs kill -9
+ echo 'app has been killed.'
+fi
+
+nohup java -cp ./logserver-es5.5.jar:/libs/*:./conf/* com.inforefiner.cloud.log.LogServer --server.port=8878 --server.servlet.contextPath=/api/logs &
+
+echo $! > pid
+
+if [ -f nohup.out ];then
+ tail -f nohup.out
+fi
