5cb3a03b7c28b795f32e89f9dc4ee54ba1ba1041
joeyhacker <jhacker7@gmail.com>
2019-04-10 14:43:01 +0800

    update

--- logserver-es5.5/src/test/java/testEs.java
+++ logserver-es5.5/src/test/java/testEs.java
@@ -1,7 +1,12 @@
 import com.inforefiner.cloud.log.utils.FileUtil;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.After;
 import org.junit.Before;
@@ -22,10 +27,15 @@ public class testEs {
 
     @Before
     public void init() throws Throwable {
+//        Settings settings = Settings.builder()
+//                .put("cluster.name", "elasticsearch").build();
         Settings settings = Settings.builder()
-                .put("cluster.name", "es5").build();
+                .put("cluster.name", "elasticsearch")
+//                .put("client.transport.sniff", false)
+                .put("xpack.security.user", "elastic:changeme")
+                .build();
         client =
-                new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.2.196"), 9300));
+                new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("info5"), 9303));
 
     }
 
@@ -33,6 +43,21 @@ public class testEs {
     public void testCreate() {
         String mapping = FileUtil.loadFromClassPath("/sync_task_log.mapping");
         client.admin().indices().prepareCreate("sync_task_log1").addMapping("sync_task_log", mapping).get();
+    }
+
+    @Test
+    public void testSearch(){
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+        boolQuery.must(QueryBuilders.termQuery("taskId", "cf1642e6-7f80-4aef-b7a6-5a4e238c8840"));
+        boolQuery.must(QueryBuilders.termQuery("logType", 0));
+        SearchResponse response =
+                client.prepareSearch("sync_task_log").setTypes("sync_task_log").setQuery(boolQuery).setFrom(0).setSize(1000).addSort("logTime", SortOrder.ASC).get();
+        Map<String, Object> ret = new HashMap();
+        List<String> logs = new ArrayList();
+        SearchHit[] hits = response.getHits().getHits();
+        for(SearchHit hit: hits){
+            System.out.println(hit.getSource().get("logText"));
+        }
     }
 
     @Test
